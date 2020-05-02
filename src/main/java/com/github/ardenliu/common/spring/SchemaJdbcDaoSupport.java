28e110efa38386211b1ba9108aa5955d57f35951
ardenliu <ardenbook@gmail.com>
2020-05-02 12:14:24 -0400

    add SchemaJdbcDaoSupport; DatabaseSchemaAware

---
+++ src/main/java/com/github/ardenliu/common/spring/SchemaJdbcDaoSupport.java
@@ -0,0 +1,25 @@
+package com.github.ardenliu.common.spring;
+
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+
+import com.github.ardenliu.common.sql.SqlUtils;
+import com.github.ardenliu.common.sql.api.DatabaseSchemaAware;
+
+public abstract class SchemaJdbcDaoSupport extends JdbcDaoSupport implements DatabaseSchemaAware {
+
+    private String schemaName;
+
+    @Override
+    public String getSchemaName() {
+        return this.schemaName;
+    }
+
+    public void setSchemaName(String schemaName) {
+        this.schemaName = schemaName;
+    }
+
+    @Override
+    public String convertSql(String sql) {
+        return SqlUtils.insertSchema(sql, getSchemaName());
+    }
+}
