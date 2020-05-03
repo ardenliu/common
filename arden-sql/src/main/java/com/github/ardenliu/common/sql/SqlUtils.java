f8abe4a2fd070bdc7da8f4ba3c3a67221a2ec19a
ardenliu <ardenbook@gmail.com>
2020-05-03 10:02:33 -0400

    add insertSchemaMap

--- arden-sql/src/main/java/com/github/ardenliu/common/sql/SqlUtils.java
+++ arden-sql/src/main/java/com/github/ardenliu/common/sql/SqlUtils.java
@@ -2,6 +2,7 @@ package com.github.ardenliu.common.sql;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.text.StringSubstitutor;
@@ -11,20 +12,36 @@ public class SqlUtils {
     /**
      * Replace the schema place holder in SQL with schema value.
      * 
-     * @param sqlTemplateString the SQL template string 
-     * @param schema the name of database schema
+     * @param sqlTemplateString the SQL template string
+     * @param schemaMap         Map.Key is schema place holder; Map.Value is schema value
+     * @return the SQL string with schema
+     */
+    public static String insertSchemaMap(String sqlTemplateString, Map<String, String> schemaMap) {
+        Map<String, String> schemaMapWithDot = schemaMap.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey, e -> getSchemaWithDot(e.getValue())));
+        StringSubstitutor sub = new StringSubstitutor(schemaMapWithDot);
+        return sub.replace(sqlTemplateString);
+    }
+
+    /**
+     * Replace the schema place holder in SQL with schema value.
+     * 
+     * @param sqlTemplateString the SQL template string
+     * @param schema            the name of database schema
      * @return the SQL string with schema
      */
     public static String insertSchema(String sqlTemplateString, String schema) {
 
-        Map<String, String> valuesMap = new HashMap<String, String>();
-        if (StringUtils.isBlank(schema)) {
-            valuesMap.put("schema", "");
-        } else {
-            valuesMap.put("schema", schema + ".");
-        }
-        StringSubstitutor sub = new StringSubstitutor(valuesMap);
+        Map<String, String> schemaMap = new HashMap<String, String>();
+        schemaMap.put("schema", schema);
 
-        return sub.replace(sqlTemplateString);
+        return insertSchemaMap(sqlTemplateString, schemaMap);
+    }
+
+    private static String getSchemaWithDot(String schema) {
+        if (StringUtils.isBlank(schema)) {
+            return "";
+        } else {
+            return schema + ".";
+        }
     }
 }
--- arden-sql/src/test/java/com/github/ardenliu/common/sql/SqlUtilsTest.java
+++ arden-sql/src/test/java/com/github/ardenliu/common/sql/SqlUtilsTest.java
@@ -2,6 +2,11 @@ package com.github.ardenliu.common.sql;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
 
 class SqlUtilsTest {
@@ -9,10 +14,25 @@ class SqlUtilsTest {
     @Test
     void insertSchema() {
         String sqlTemplateString = "select * from ${schema}table_a where id = ?";
-        
+
         assertEquals("select * from test.table_a where id = ?", SqlUtils.insertSchema(sqlTemplateString, "test"));
         assertEquals("select * from table_a where id = ?", SqlUtils.insertSchema(sqlTemplateString, "  "));
         assertEquals("select * from table_a where id = ?", SqlUtils.insertSchema(sqlTemplateString, ""));
         assertEquals("select * from table_a where id = ?", SqlUtils.insertSchema(sqlTemplateString, null));
+    }
+
+    @Test
+    void insertSchemaMap() {
+        String sqlTemplateString = "select * from ${schema0}table_a a, ${schema1}table_b b where a.id = b.id";
+
+        Map<String, String> schemaMap = Stream.of(new String[][] { { "schema0", "test0" }, { "schema1", "test1" }, }).collect(Collectors.toMap(data -> data[0], data -> data[1]));
+
+        assertEquals("select * from test0.table_a a, test1.table_b b where a.id = b.id", SqlUtils.insertSchemaMap(sqlTemplateString, schemaMap));
+
+        schemaMap = new HashMap<String, String>();
+        schemaMap.put("schema0", "test0");
+        schemaMap.put("schema1", null);
+
+        assertEquals("select * from test0.table_a a, table_b b where a.id = b.id", SqlUtils.insertSchemaMap(sqlTemplateString, schemaMap));
     }
 }
