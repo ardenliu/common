ad6ec5c2d3b806a82650dc751d457cafd2d5313a
ardenliu <ardenbook@gmail.com>
2020-05-03 14:37:36 -0400

    add equals() to compare two json string

--- arden-jackson/src/main/java/com/github/ardenliu/common/jackson/JacksonUtils.java
+++ arden-jackson/src/main/java/com/github/ardenliu/common/jackson/JacksonUtils.java
@@ -1,9 +1,14 @@
 package com.github.ardenliu.common.jackson;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JacksonUtils {
+    private static final Logger logger = LogManager.getLogger(JacksonUtils.class);
 
     /**
      * Create JSON String from one object
@@ -16,7 +21,40 @@ public class JacksonUtils {
         try {
             return mapper.writeValueAsString(object);
         } catch (JsonProcessingException e) {
+            logger.error("Got JsonProcessingException", e);
             throw new RuntimeException(e.getMessage(), e);
         }
+    }
+
+    /**
+     * Using input ObjectMapper to compares two JSON string, returning true if they are full (deep) value equality.
+     * 
+     * @param mapper
+     * @param json1  the first JSON string
+     * @param json2  the second JSON string
+     * @return
+     */
+    public static boolean equals(ObjectMapper mapper, String json1, String json2) {
+        try {
+            return mapper.readTree(json1).equals(mapper.readTree(json2));
+        } catch (JsonMappingException e) {
+            logger.error("Got JsonMappingException", e);
+            throw new RuntimeException(e.getMessage(), e);
+        } catch (JsonProcessingException e) {
+            logger.error("Got JsonProcessingException", e);
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Compares two JSON string, returning true if they are full (deep) value equality.
+     * 
+     * @param json1 the first JSON string
+     * @param json2 the second JSON string
+     * @return
+     */
+    public static boolean equals(String json1, String json2) {
+        ObjectMapper mapper = new ObjectMapper();
+        return JacksonUtils.equals(mapper, json1, json2);
     }
 }
--- arden-jackson/src/test/java/com/github/ardenliu/common/jackson/JacksonUtilsTest.java
+++ arden-jackson/src/test/java/com/github/ardenliu/common/jackson/JacksonUtilsTest.java
@@ -1,7 +1,12 @@
 package com.github.ardenliu.common.jackson;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,5 +20,13 @@ public class JacksonUtilsTest {
         String jsonString = JacksonUtils.objectToJsonString(testMap);
 
         assertEquals("{\"city\":\"new york\"}", jsonString);
+    }
+    
+    @Test
+    void equals() throws IOException, URISyntaxException {
+
+        String json1 = new String(Files.readAllBytes(Paths.get(JacksonUtilsTest.class.getClassLoader().getResource("com/github/ardenliu/common/jackson/test1.json").toURI())));
+        String json2 = new String(Files.readAllBytes(Paths.get(JacksonUtilsTest.class.getClassLoader().getResource("com/github/ardenliu/common/jackson/test2.json").toURI())));
+        assertTrue(JacksonUtils.equals(json1, json2));
     }
 }
