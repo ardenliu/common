a4cd1800a8867e03a26150bd752c532210822bfd
ardenliu <ardenbook@gmail.com>
2020-05-08 21:38:22 -0400

    improve

--- arden-google-http-client/src/main/java/com/github/ardenliu/common/googlehttpclient/GoogleHttpUtils.java
+++ arden-google-http-client/src/main/java/com/github/ardenliu/common/googlehttpclient/GoogleHttpUtils.java
@@ -1,10 +1,14 @@
 package com.github.ardenliu.common.googlehttpclient;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Path;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.commons.io.FileUtils;
 
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
@@ -26,12 +30,16 @@ public class GoogleHttpUtils {
     public static InputStream getContentAsInputStream(String urlString) {
         return GoogleHttpUtils.getContentAsInputStream(new GenericUrl(urlString));
     }
-    
-    public static InputStream getContentAsInputStream(GenericUrl genericUrl) {
+
+    public static InputStream getContentAsInputStream(URL url) {
+        return GoogleHttpUtils.getContentAsInputStream(new GenericUrl(url));
+    }
+
+    private static InputStream getContentAsInputStream(GenericUrl genericUrl) {
         return GoogleHttpUtils.getContentAsInputStream(genericUrl, 0);
     }
 
-    public static InputStream getContentAsInputStream(GenericUrl genericUrl, int timeout) {
+    private static InputStream getContentAsInputStream(GenericUrl genericUrl, int timeout) {
         // TODO: handle local file
 //      String protocol = genericUrl.toURL().getProtocol();
 //      if ("file".equals(protocol)) {
@@ -53,7 +61,15 @@ public class GoogleHttpUtils {
         }
     }
 
-    public static String getContentAsString(GenericUrl genericUrl) {
+    public static String getContentAsString(URL url) {
+        return GoogleHttpUtils.getContentAsString(new GenericUrl(url));
+    }
+
+    public static String getContentAsString(String urlString) {
+        return GoogleHttpUtils.getContentAsString(new GenericUrl(urlString));
+    }
+
+    private static String getContentAsString(GenericUrl genericUrl) {
         try {
             HttpRequestFactory requestFactory = new NetHttpTransport().createRequestFactory();
             HttpRequest request = requestFactory.buildGetRequest(genericUrl);
@@ -61,6 +77,23 @@ public class GoogleHttpUtils {
             return response.parseAsString();
         } catch (IOException e) {
             logger.error(e.getMessage(), e);
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    public static void saveUrlIntoFile(URL url, Path targetFilePath) {
+        GoogleHttpUtils.saveUrlIntoFile(new GenericUrl(url), targetFilePath.toString());
+    }
+
+    public static void saveUrlIntoFile(String url, String targetFilePath) {
+        GoogleHttpUtils.saveUrlIntoFile(new GenericUrl(url), targetFilePath);
+    }
+
+    private static void saveUrlIntoFile(GenericUrl genericUrl, String targetFilePath) {
+        try (InputStream inputStreamFromUrl = GoogleHttpUtils.getContentAsInputStream(genericUrl)) {
+            File targetFile = new File(targetFilePath);
+            FileUtils.copyInputStreamToFile(inputStreamFromUrl, targetFile);
+        } catch (IOException e) {
             throw new RuntimeException(e.getMessage(), e);
         }
     }
