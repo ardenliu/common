cd999b89f2aaad458320fcb7a25f75235e2cd738
ardenliu <ardenbook@gmail.com>
2020-05-14 23:48:16 -0400

    copyFromClassPath

---
+++ arden-file/src/main/java/com/github/ardenliu/common/file/ResourcesUtils.java
@@ -0,0 +1,44 @@
+package com.github.ardenliu.common.file;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+public class ResourcesUtils {
+    private static final Logger logger = LogManager.getLogger(ResourcesUtils.class);
+
+    public static void copyFromClassPath(final String resourcePath, final Path targetRoot) throws IOException {
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+
+        Resource[] resources = resolver.getResources(ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + resourcePath + "/**");
+
+        for (Resource resource : resources) {
+            if (resource.exists()) {
+                String pathOfResource = resource.getURL().getPath();
+                int lastIndex = pathOfResource.lastIndexOf(resourcePath);
+                if (lastIndex == -1) {
+                    logger.error("cannot handle pathOfResource[{}]", pathOfResource);
+                    continue;
+                }
+                String relativePath = pathOfResource.substring(lastIndex, pathOfResource.length());
+                Path tergetPath = Paths.get(targetRoot.toString(), relativePath);
+                if (resource.isReadable() & resource.isFile()) {
+                    logger.debug("creating file[{}]", tergetPath);
+                    File tergetFile = tergetPath.toFile();
+                    org.apache.commons.io.FileUtils.copyURLToFile(resource.getURL(), tergetFile);
+
+                } else if (resource.getFile().isDirectory()) {
+                    logger.debug("creating directory[{}]", tergetPath);
+                    Files.createDirectories(tergetPath);
+                }
+            }
+        }
+    }
+}
---
+++ arden-file/src/test/java/com/github/ardenliu/common/file/ResourcesUtilsTest.java
@@ -0,0 +1,27 @@
+package com.github.ardenliu.common.file;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+class ResourcesUtilsTest {
+
+    @Test
+    void copyFromClassPath(@TempDir Path tempDir) throws IOException {
+        ResourcesUtils.copyFromClassPath("com/github/ardenliu/common/file/resourcestest", tempDir);
+
+        Path txt1File = Paths.get(tempDir.toString(), "com/github/ardenliu/common/file/resourcestest", "1.txt");
+        String contentOfTxt1File = org.apache.commons.io.FileUtils.readFileToString(txt1File.toFile() , StandardCharsets.UTF_8);
+        assertEquals("111", contentOfTxt1File);
+
+        Path txt2File = Paths.get(tempDir.toString(), "com/github/ardenliu/common/file/resourcestest/folder", "2.txt");
+        String contentOfTxt2File = org.apache.commons.io.FileUtils.readFileToString(txt2File.toFile() , StandardCharsets.UTF_8);
+        assertEquals("222", contentOfTxt2File);
+    }
+}
